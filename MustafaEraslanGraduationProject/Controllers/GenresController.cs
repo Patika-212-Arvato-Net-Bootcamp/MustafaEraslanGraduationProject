36cfb2b3889cbc39ac38076876be74ac1d0f9076
Mustafa Eraslan <44713722+MustafaEraslan@users.noreply.github.com>
2022-07-07 14:53:20 +0300

    created

---
+++ MustafaEraslanGraduationProject/Controllers/GenresController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MustafaEraslanGraduationProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        // GET: api/<GenresController>
+        [HttpGet]
+        public IEnumerable<string> ListGenres()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+
+        // POST api/<GenresController>
+        [HttpPost]
+        public void AddGenre([FromBody] string genre)
+        {
+            //string yerine Genre entities oluşturulacak. O nesne buraya verilecek.
+        }
+
+        // PUT api/<GenresController>/5
+        [HttpPut("{id}")]
+        public void UpdateGenre(int id, [FromBody] string genre)
+        {
+            //string yerine Genre entities oluşturulacak. O nesne buraya verilecek.value değerleriküçük.
+        }
+
+        // DELETE api/<GenresController>/5
+        [HttpDelete("{id}")]
+        public void DeleteGenre(int id)
+        {
+        }
+    }
+}
---
+++ MustafaEraslanGraduationProject/Controllers/MoviesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MustafaEraslanGraduationProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        // GET: api/<Movies>
+        [HttpGet]
+        public IEnumerable<string> GetMovieDetail()
+        {
+            return new string[] { "" };
+        }
+
+        // GET api/<Movies>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<Movies>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<Movies>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<Movies>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
